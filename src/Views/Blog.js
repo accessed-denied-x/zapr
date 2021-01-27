92b947e339716a0c3461b97035aa3ba70b7e8833
The-Real-Guy <aasdimaranan@gmail.com>
2021-01-26 21:23:31 -0700

    Minor change: blog component now shows author

--- src/Views/Blog.js
+++ src/Views/Blog.js
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect } from 'react'
 import { GlobalContext } from '../Context/GlobalState';
 import { useParams } from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Blog() {
 	const { blog, getBlog } = useContext(GlobalContext);
+	const { user, isLoading } = useAuth0();
 
 	const { id } = useParams();
 
@@ -17,6 +19,7 @@ export default function Blog() {
 		<div>
 			<div className="blog-header">
 				<p>{blog.title}</p>
+				<p>{"Author: " + user.name}</p>
 				<p>{blog.description}</p>
 			</div>
 			<div>
