eef831a9e8c0349b5bdd8d7d5d925acf6547da5a
kito0 <drewshah0@gmail.com>
2021-02-12 13:01:25 -0700

    styled new blog form

--- src/Views/NewBlog.js
+++ src/Views/NewBlog.js
@@ -1,51 +1,100 @@
 import React, { useState, useContext } from 'react';
 import { GlobalContext } from '../Context/GlobalState';
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from '@auth0/auth0-react';
+import { useStyles } from '../Styles/NewBlogStyles';
+import { Card, TextField, Button } from '@material-ui/core';
 
 export default function NewBlog() {
-    const [title, setTitle] = useState('');
-    const [description, setDesc] = useState('');
-    const [body, setBodyText] = useState('');
-    const [submitted, setSubmitted] = useState(null);
-    const { user, isAuthenticated, isLoading } = useAuth0();
+	const [title, setTitle] = useState('');
+	const [description, setDesc] = useState('');
+	const [body, setBodyText] = useState('');
+	const { user, isAuthenticated, isLoading } = useAuth0();
+	const classes = useStyles();
 
-    const { addTransaction } = useContext(GlobalContext);
+	const { addTransaction } = useContext(GlobalContext);
 
-    if (isLoading) {
-        return <div>Loading ...</div>;
-      }
+	if (isLoading) {
+		return <div>Loading ...</div>;
+	}
 
-    const onSubmit = e => {
-        e.preventDefault();
+	const onSubmit = (e) => {
+		e.preventDefault();
+		const newTransaction = {
+			title,
+			description,
+			body,
+			user: user.name,
+		};
+		addTransaction(newTransaction);
+	};
 
-        const newTransaction = {
-            title,
-            description,
-            body,
-            user: user.name
-        }
+	console.log('NEW BLOG PAGE RENDERED');
 
-        addTransaction(newTransaction)
-    }
-
-    console.log('NEW BLOG PAGE RENDERED')
-
-    return isAuthenticated && (
-        <form onSubmit={onSubmit}>
-            <label htmlFor="title">Title: </label>
-            <input type="text" value={submitted ? "Submitted!" : title} onChange={(e) => setTitle(e.target.value)} required />
-            
-            <label htmlFor="description">Description: </label>
-            <input type="text" value={submitted ? "Submitted!" : description} onChange={(e) => setDesc(e.target.value)} required />
-
-            <label htmlFor="body">Body: </label>
-            <textarea placeholder="Let inspiration strike..." value={submitted ? "Submitted!" : body} onChange={(e) => setBodyText(e.target.value)} required></textarea>
-
-            <button onClick={() => {
-                setSubmitted(true);
-            }}>
-                Submit
-            </button>
-        </form>
-    )
+	return (
+		isAuthenticated && (
+			<div className={classes.newblog}>
+				<Card className={classes.card}>
+					<div className={classes.box}>
+						<form onSubmit={onSubmit}>
+							<TextField
+								variant="filled"
+								label="Title"
+								color="secondary"
+								onChange={(e) => setTitle(e.target.value)}
+								className={classes.textfield}
+								required
+								fullWidth
+								InputProps={{
+									className: classes.input,
+								}}
+								InputLabelProps={{
+									className: classes.input,
+								}}
+							/>
+							<TextField
+								variant="filled"
+								label="Description"
+								color="secondary"
+								onChange={(e) => setDesc(e.target.value)}
+								className={classes.textfield}
+								required
+								fullWidth
+								InputProps={{
+									className: classes.input,
+								}}
+								InputLabelProps={{
+									className: classes.input,
+								}}
+							/>
+							<TextField
+								variant="outlined"
+								label="Body"
+								color="secondary"
+								onChange={(e) => setBodyText(e.target.value)}
+								className={classes.textfield}
+								required
+								fullWidth
+								multiline
+								rows="10"
+								InputProps={{
+									className: classes.input,
+								}}
+								InputLabelProps={{
+									className: classes.input,
+								}}
+							/>
+							<Button
+								variant="contained"
+								type="submit"
+								color="secondary"
+								className={classes.button}
+							>
+								Post
+							</Button>
+						</form>
+					</div>
+				</Card>
+			</div>
+		)
+	);
 }
