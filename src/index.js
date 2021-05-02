0305b3052cfb892ec89851a0e2ff58256b738df2
The-Real-Guy <aasdimaranan@gmail.com>
2021-05-02 04:22:47 -0600

    Started bare bones of implementing redux toolkit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
+import store from './store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
 	<Auth0Provider
@@ -10,7 +12,9 @@ ReactDOM.render(
 		clientId="ZcMjaUtV1COyXj6D78AT6s6ZJCpcmcIr"
 		redirectUri={window.location.origin}
 	>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</Auth0Provider>,
 	document.getElementById('root')
 );
